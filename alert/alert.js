9dd20f9437e980074a955f7926395f051105faf6
kunkenshou <kunkenshou@gmail.com>
2024-11-08 23:52:23 +0900

    アラート機能を実装

--- alert/alert.js
+++ alert/alert.js
@@ -1,24 +1,32 @@
 
-
-
 addEventListener("DOMContentLoaded", (event) => {
 
   const startTime = document.getElementById("startTime");
   const valueSpan = document.getElementById("value");
-  const btn = document.getElementById("btn");
-  const reset = document.getElementById("reset");
+  let alert_boolean = new Boolean(true);
+  let check_value = document.getElementById("No1");
+  check_value.checked = false;
+  let check_boolean = new Boolean(false); 
+  const navi = document.getElementById("navi");
 
-  btn.addEventListener("click", (event) => {
-    valueSpan.innerText = startTime.value;
+
+  //ダイアログ関係
+  const dialog = document.querySelector("dialog");
+  const closeButton = document.querySelector("dialog button");
+
+
+//[閉じる]ボタンでダイアログを閉じる
+  closeButton.addEventListener("click", ()=> {
+	check_value.checked = false;
+	navi.style.display = "none";
+	dialog.close();
   });
-
-  reset.addEventListener("click", (event) => {
-   valueSpan.innerText = "";
-  });
-
+  
   let nIntervId = setInterval(time, 600);
   
   function time(){
+  let check_value = document.getElementById("No1");
+  check_boolean = check_value.checked;
   const today = new Date();
   const hours = today.getHours();
   const minutes = today.getMinutes();
@@ -47,31 +55,61 @@ addEventListener("DOMContentLoaded", (event) => {
   const time = document.getElementById("time");
   time.innerText = `${time_arry[0]}時${time_arry[1]}分${time_arry[2]}秒`
  
-  const alert_time = document.getElementById("value");
-  const navi = document.getElementById("navi");
+
+
   const time_now = `${time_arry[0]}:${time_arry[1]}`
-  const str_alerttime = alert_time.innerText;
 
-  function normal(){
-    navi.innerHTML = "時間ではありません";
-  }
+	  
+  const btn = document.getElementById("btn");
+  const reset = document.getElementById("reset");
+  valueSpan.innerText = startTime.value;
+  const alert_time = document.getElementById("value");
+  let str_alerttime = alert_time.innerText; 
 
-  function alert(){
-    console.log("hgoe");
-    navi.innerHTML = "時間です";
-  }
 
+
+//[真偽値を代入]現在の時刻と設定したアラームが同じか評価して真偽値を代入する
   if(time_now === str_alerttime) {
-    alert();
-    /*
-    console.log("時間です");
-    console.log(time_now);
-    console.log(str_alerttime);
-    window.alert("時間です");
-    */
+    alert_boolean = new Boolean(true);
   }else{
-    normal();
+    alert_boolean = new Boolean(false);
+}
+
+
+//[アラーム時間が登録されてない時の処理]
+  function normal_view(){
+    navi.style.display = "none";
   }
 
-}
+//[アラーム時間の処理]ダイアログボックスに表示
+  function dialog_view(){
+    dialog.showModal();
+    navi.style.display = "grid";
+    navi.style.position = "relative";
+    navi.style.background = "rgb(207 232 220)";
+    navi.style.margin = "auto";
+    navi.style.width = "150px";
+    navi.style.height = "100px";
+    navi.style.border = "5px";
+    navi.style.padding = "1em";
+  }
+
+
+//[チェックボタン]ボタンでチェック、未チェックで動作を変える。
+  check_value.addEventListener("click", (event) => {
+	check_boolean = check_value.checked;
+
+  });
+
+
+  if (check_boolean == true && alert_boolean == true) {
+		dialog_view();
+
+	}else{
+		normal_view();
+
+	}
+  }
+
+
   });
